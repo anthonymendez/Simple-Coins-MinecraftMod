0d20d33e145239b55dae457cc331459dc8fa36e0
tonydetiger <anthonymendez9@gmail.com>
2017-01-12 17:24:15 -0500

    Changelog added

--- src/java/com/TonyTiger/simplecoins/ClientProxy.java
+++ src/java/com/TonyTiger/simplecoins/ClientProxy.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.registry.VillagerRegistry;
 
 public class ClientProxy {
 	public void preInit(FMLPreInitializationEvent e){
