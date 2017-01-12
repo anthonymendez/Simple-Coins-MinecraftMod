3b70f09aa019b6697b2e3c700e9ca5e4098fcb69
tonydetiger <anthonymendez9@gmail.com>
2017-01-11 23:25:44 -0500

    0.4.0 - Coins added to mob drops.
    Zombies, ZombiePigmen, Husks, and ZombieVillagers have a 20% chance of
    dropping 4 iron coins, and a 10% chance of dropping 4 coins.
    Evokers, Vindicators and Villagers all drop 0-3 iron coinsand 0-3 gold
    coins

--- src/java/com/TonyTiger/simplecoins/Main.java
+++ src/java/com/TonyTiger/simplecoins/Main.java
@@ -16,7 +16,7 @@ public class Main {
 	
 	public static final String MODID = "simplecoins";
 	public static final String MODNAME = "Simple Coins mod";
-	public static final String VERSION = "0.3.2";
+	public static final String VERSION = "0.4.0";
 	
 	@Instance(MODID)
     public static Main instance = new Main();
