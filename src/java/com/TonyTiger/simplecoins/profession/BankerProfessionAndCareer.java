5feabdc209d6d387ad25a26feb23374c6ef05071
tonydetiger <anthonymendez9@gmail.com>
2017-01-13 00:46:19 -0500

    0.5.1 - Emerald to coins trade option removed
    Emeralds cannot be converted to coins properly. This trade option has
    been removed for the time being.
    That means this trade is what is left:
    At Tier 1:
    21-27 Iron Coins -> 1 Emerald
    24-30 Gold Coins -> 1 Emerald
    At Tier 2:
    21-27 Iron Coins -> 1 Emerald
    24-30 Gold Coins -> 1 Emerald

--- src/java/com/TonyTiger/simplecoins/profession/BankerProfessionAndCareer.java
+++ src/java/com/TonyTiger/simplecoins/profession/BankerProfessionAndCareer.java
@@ -20,19 +20,19 @@ public class BankerProfessionAndCareer {
 	public static ITradeList[] trades = {
 			new EmeraldForItems(ModItems.IRONCOIN, new PriceInfo(21,27)),
 			new EmeraldForItems(ModItems.GOLDCOIN, new PriceInfo(24,30)),
-			new EntityVillager.ListItemForEmeralds(new ItemStack(ModItems.IRONCOIN,RandNum(21,27)), new PriceInfo(1,1)),
-			new EntityVillager.ListItemForEmeralds(new ItemStack(ModItems.GOLDCOIN,RandNum(24,30)), new PriceInfo(1,1))
+//			new EntityVillager.ListItemForEmeralds(new ItemStack(ModItems.IRONCOIN,21), new PriceInfo(1,1)),
+//			new EntityVillager.ListItemForEmeralds(new ItemStack(ModItems.GOLDCOIN,24), new PriceInfo(1,1)),
 	};
 	public static ITradeList[] tradesl2 = {
 			new EmeraldForItems(ModItems.IRONCOIN, new PriceInfo(19,25)),
 			new EmeraldForItems(ModItems.GOLDCOIN, new PriceInfo(22,28)),
-			new EntityVillager.ListItemForEmeralds(new ItemStack(ModItems.IRONCOIN,RandNum(38,50)), new PriceInfo(2,2)),
-			new EntityVillager.ListItemForEmeralds(new ItemStack(ModItems.GOLDCOIN,RandNum(44,56)), new PriceInfo(2,2))
+//			new EntityVillager.ListItemForEmeralds(new ItemStack(ModItems.IRONCOIN,RandNum(38,50)), new PriceInfo(2,2)),
+//			new EntityVillager.ListItemForEmeralds(new ItemStack(ModItems.GOLDCOIN,RandNum(44,56)), new PriceInfo(2,2))
 	};
 	
 	public static void initilize(){
 		BankerProfessionAndCareer.bankerCar.addTrade(1, BankerProfessionAndCareer.trades);
-		BankerProfessionAndCareer.bankerCar.addTrade(2, BankerProfessionAndCareer.tradesl2);
+//		BankerProfessionAndCareer.bankerCar.addTrade(2, BankerProfessionAndCareer.tradesl2);
 		VillagerRegistry.instance().register(BankerProfessionAndCareer.bankerProf);
 	}
 	
